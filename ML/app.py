a57054ba80ad0a2b27008a2c4a077456645d6a11
clarkkkkk <juanfarrel0404@gmail.com>
2024-12-14 03:35:48 +0700

    feat: add model retraining endpoint and functionality

--- ML/app.py
+++ ML/app.py
@@ -3,6 +3,7 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
 import numpy as np
 import os
+from tensorflow.keras.optimizers import Adam
 
 # Inisialisasi Flask
 app = Flask(__name__)
@@ -36,17 +37,67 @@ def predict():
         predicted_class = np.argmax(predictions, axis=1)
 
         # Hapus gambar setelah diproses
-        os.remove(image_path)
+        # os.remove(image_path)
 
         # Mapping hasil prediksi ke label
         label = "AI Generated" if predicted_class[0] == 0 else "Human-made"
-
+        
         # Return hasil prediksi
         return jsonify({
             'predicted_class': int(predicted_class[0]),
             'label': label,
-            'probabilities': predictions.tolist()
+            'probabilities': predictions.tolist(),
+            'imagePath': image_path,
         })
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+def retrain_model(retrain_data):
+    images = []
+    labels = []
+
+    for data in retrain_data:
+        image_path = data['imagepath']
+        new_class = data['new_class']
+        
+        # Load gambar dan ubah ukuran sesuai model input
+        img = load_img(image_path, target_size=(32, 32))  # Sesuaikan dengan ukuran input model
+        img_array = img_to_array(img)
+        img_array = img_array / 255.0  # Normalisasi (jika diperlukan)
+        images.append(img_array)
+        
+        # Tambahkan label ke array
+        labels.append(new_class)
+    
+    # Mengubah list ke numpy array
+    X_train = np.array(images)
+    y_train = np.array(labels)
+    
+    # Compile ulang model sebelum retraining
+    model.compile(optimizer=Adam(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    # Melatih model dengan data baru
+    model.fit(X_train, y_train, epochs=5, batch_size=32)
+
+    # Simpan model yang sudah dilatih ulang
+    model.save(MODEL_PATH)
+
+    print("Retraining completed.")
+
+# Endpoint untuk retraining model
+@app.route('/retrain-model', methods=['POST'])
+def retrain():
+    try:
+        # Dapatkan data retrain dari request JSON
+        retrain_data = request.get_json()
+
+        if not retrain_data:
+            return jsonify({'error': 'No retrain data provided'}), 200
+
+        # Melakukan retraining
+        retrain_model(retrain_data)
+        
+        return jsonify({'message': 'Model retrained successfully'}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
